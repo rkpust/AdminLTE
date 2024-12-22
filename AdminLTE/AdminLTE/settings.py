0f4a6ddd225065848cab1033848a97b67109e232
rkpust <rezaul.cse.pust17@gmail.com>
2024-12-22 17:56:15 +0600

    added installed_app and configured the urls

--- AdminLTE/AdminLTE/settings.py
+++ AdminLTE/AdminLTE/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_adminlte.apps.AdminAdminlteConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
